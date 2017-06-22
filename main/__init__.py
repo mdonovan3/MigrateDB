cd5e2599a70126248fc936e11e5cee4bc3faff72
mdonovan3 <martindonovan3@gmail.com>
2017-06-22 16:31:53 -0400

    commit

--- main/__init__.py
+++ main/__init__.py
@@ -1,14 +1,18 @@
 import psycopg2
+from psycopg2 import extras
 
 hostname = '192.168.130.9'
 username = 'inventory'
 password = '1261brg'
 database = 'inventory'
+wine_category_id = 1
 
 conn = psycopg2.connect( host=hostname, user=username, password=password, dbname=database )
-cur = conn.cursor()
-cur.execute( "SELECT inventory_name FROM product_instances" )
-for inv_name in cur.fetchall() :
-  print (inv_name)
+cur = conn.cursor(cursor_factory=psycopg2.extras.DictCursor)
+cur.execute( "SELECT * FROM products p, product_instances pi where p.product_id = pi.product_id \
+and p.major_category_id = 1" )
+for row in cur.fetchall() :
+    print("%s, %s" % (row['inventory_name'], row['product_name']))
+    
     
     
